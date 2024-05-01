575778470d9992975f1fde588e688c15b0f60ff1
Neeta Buhecha <neeta.b@gmail.com>
2024-05-01 11:16:45 +0100

    Initial Commit

---
+++ fiddling/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fiddling
+//
+//  Created by Neeta Buhecha on 01/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fiddling/fiddlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  fiddlingApp.swift
+//  fiddling
+//
+//  Created by Neeta Buhecha on 01/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct fiddlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
