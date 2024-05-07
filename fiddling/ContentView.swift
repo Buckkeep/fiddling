7deba6831635c1c32d1c4d9adfe1bf52d680d2ba
Neeta Buhecha <neeta.b@gmail.com>
2024-05-07 11:37:05 +0100

    Practising segment style button in SwiftUI - toggles on/off, fills space across screen like segment picker.

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -7,28 +7,52 @@
 
 import SwiftUI
 
+struct Option: Hashable {
+    let label: String
+    var selected: Bool
+}
+
+
 struct ContentView: View {
+    
+    @State var options: [Option] = [
+        Option(label: "Dot All", selected: false),
+        Option(label: "Extended", selected: false),
+        Option(label: "Ignore Case", selected: false),
+        Option(label: "Multi-line", selected: false),
+    ]
+    
     var body: some View {
-            Image("frodo")
-            Image(decorative: "frodo")
-            Image(systemName: "pencil.circle")
-                .foregroundStyle(.red)
-                .font(.largeTitle)
+        OptionPicker(options: $options)
+    }
+}
 
-        Button {
-            print("Button was tapped")
-        } label: {
-            Label("Edit", systemImage: "pencil")
-                .padding()
-                .foregroundStyle(.white)
-                .background(.red)
-        }
-        Button() {
-            print("Button was pressed")
-        } label: {
-            Image("frodo")
-        }
 
+struct OptionPicker: View {
+    
+    @Binding var options: [Option]
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            ForEach(options.indices, id: \.self) { i in
+                
+                Text(options[i].label)
+                    .padding(.horizontal, 4)
+                    .padding(5)
+                    .background(options[i].selected ? .blue : .gray)
+                
+                    .onTapGesture {
+                        options[i].selected.toggle()
+                    }
+                
+                if i < options.count-1 {
+                Divider()
+                }
+            }
+        }
+        .foregroundColor(.white)
+        .cornerRadius(10)
+        .frame(height: 32)
     }
 }
 
