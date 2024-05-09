76275628b3fd4d7e7c269d9978eb929b8e7f38a0
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-09 10:19:38 +0100

    Formatted the stepper number, added a min-max range and added a step of 0.25.

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     
     var body: some View {
-        Stepper("\(sleepAmount) hours", value: $sleepAmount)
+        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
     }
 }
 
