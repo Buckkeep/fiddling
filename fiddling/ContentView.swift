416e5f56867630096a3d26e18c7643ae04df5d03
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-09 09:39:52 +0100

    Basic Stepper

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -7,54 +7,18 @@
 
 import SwiftUI
 
-struct Option: Hashable {
-    let label: String
-    var selected: Bool
-}
 
 
 struct ContentView: View {
-    
-    @State var options: [Option] = [
-        Option(label: "Dot All", selected: false),
-        Option(label: "Extended", selected: false),
-        Option(label: "Ignore Case", selected: false),
-        Option(label: "Multi-line", selected: false),
-    ]
+    @State private var sleepAmount = 8.0
     
     var body: some View {
-        OptionPicker(options: $options)
+        Stepper("\(sleepAmount) hours", value: $sleepAmount)
     }
 }
 
 
-struct OptionPicker: View {
-    
-    @Binding var options: [Option]
-    
-    var body: some View {
-        HStack(spacing: 0) {
-            ForEach(options.indices, id: \.self) { i in
-                
-                Text(options[i].label)
-                    .padding(.horizontal, 4)
-                    .padding(5)
-                    .background(options[i].selected ? .blue : .gray)
-                
-                    .onTapGesture {
-                        options[i].selected.toggle()
-                    }
-                
-                if i < options.count-1 {
-                Divider()
-                }
-            }
-        }
-        .foregroundColor(.white)
-        .cornerRadius(10)
-        .frame(height: 32)
-    }
-}
+
 
 #Preview {
     ContentView()
