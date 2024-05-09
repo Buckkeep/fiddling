61378cc8b91d20b4ffcb34c992609f34d54e219a
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-09 11:34:59 +0100

    Date formatting, remembering to nil coalesce with values returned from Date object because they are optionals

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -15,11 +15,23 @@ struct ContentView: View {
     @State private var wakeUp = Date.now
     
     var body: some View {
-        Text("\(wakeUp)")
-        DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
-            .labelsHidden()
-        DatePicker("Please enter a date", selection: $wakeUp, in:Date.now...)
-            .labelsHidden()
+
+        Text(Date.now, format: .dateTime.hour().minute())
+        
+        Text(Date.now.formatted(date: .long, time: .shortened))
+
+        
+    }
+    
+    func exampleDates() {
+//        var components = DateComponents()
+//        components.hour = 8
+//        components.minute = 0
+//        let date = Calendar.current.date(from: components) ?? .now
+        
+        let components = Calendar.current.dateComponents([.hour,.minute], from: .now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
     }
 }
 
