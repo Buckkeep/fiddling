e15bb086244e0d0deaf4c1d6d45f3e7699879b1b
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-10 15:02:18 +0100

    Introduction to Lists and mixing static and dynamic content within Lists

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -12,26 +12,20 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @State private var wakeUp = Date.now
+    let people = ["Finn", "Leia", "Luke", "Rey"]
     
     var body: some View {
+        
+            List {
+                Text("Scooby Doo, where are you?")
+                
+                ForEach(people, id: \.self) {
+                    Text($0)
+                }
+                
+                Text("Donde estas, Scooby Doo?")
+            }
 
-        Text(Date.now, format: .dateTime.hour().minute())
-        
-        Text(Date.now.formatted(date: .long, time: .shortened))
-
-        
-    }
-    
-    func exampleDates() {
-//        var components = DateComponents()
-//        components.hour = 8
-//        components.minute = 0
-//        let date = Calendar.current.date(from: components) ?? .now
-        
-        let components = Calendar.current.dateComponents([.hour,.minute], from: .now)
-        let hour = components.hour ?? 0
-        let minute = components.minute ?? 0
     }
 }
 
