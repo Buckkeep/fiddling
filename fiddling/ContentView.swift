95c475280cc6ab2b3302e969fde77ee6bed05cff
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-21 15:32:52 +0100

    Deleting items using onDelete()

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -22,17 +22,36 @@ struct SecondView: View {
 
 struct ContentView: View {
     
-    @State private var showingSheet = false
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
     
     var body: some View {
-        Button("Show Sheet") {
-            showingSheet.toggle()
-        }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "Buckkeep")
+        NavigationStack {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add Number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .toolbar {
+                EditButton()
+            }
         }
     }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
+        }
+
 }
+
 
 
 
