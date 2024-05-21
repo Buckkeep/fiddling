26f2c9f1facf7228ce9d9d75e515a2dcf6bfeed0
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-21 15:52:31 +0100

    Storing user settings with UserDefaults (via AppStorage wrapper) (part 2 of 2)

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -22,13 +22,12 @@ struct SecondView: View {
 
 struct ContentView: View {
     
-    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
+    @AppStorage("tapCount") private var tapCount = 0
     
     var body: some View {
         Button("Tap Count: \(tapCount)") {
             tapCount += 1
             
-            UserDefaults.standard.set(tapCount, forKey: "Tap")
         }
     }
 
