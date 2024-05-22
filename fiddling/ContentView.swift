cb0407cc468055939a53a2d0ed342c4f8c23ab55
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-22 16:14:09 +0100

    Basic NavigationLink setup

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -4,38 +4,25 @@
 //
 //  Created by Neeta Buhecha on 01/05/2024.
 //
-//  Selecting dates and times with DatePicker
+//  Pushing new views onto the stack using NavigationLink
 //
 
 import SwiftUI
 
-struct User: Codable {
-    let firstname: String
-    let lastname: String
-}
-
 struct ContentView: View {
     
-    @State private var user = User(firstname: "Taylor", lastname: "Swift")
-    
-    
     var body: some View {
-        Button("Save User") {
-            let encoder = JSONEncoder()
-            
-            if let data = try? encoder.encode(user) {
-                UserDefaults.standard.set(data, forKey: "UserData")
+        NavigationStack {
+            NavigationLink("Tap Me") {
+                Text("Detail View")
             }
+                .navigationTitle("SwiftUI")
         }
             
     }
 
 
 }
-
-
-
-
 
 #Preview {
     ContentView()
