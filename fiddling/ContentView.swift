1d88191bbd89be17c1f229d5caeeb18776f07a7a
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-23 11:11:16 +0100

    Working with hierarchical Codable data

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -4,21 +4,40 @@
 //
 //  Created by Neeta Buhecha on 01/05/2024.
 //
-//  Pushing new views onto the stack using NavigationLink
+//  Working with hierarchical Codable data
 //
 
 import SwiftUI
 
+struct User: Codable {
+    let name: String
+    let address: Address
+}
+
+struct Address: Codable {
+    let street: String
+    let city: String
+}
+
 struct ContentView: View {
     
     var body: some View {
-        NavigationStack {
-            List(0..<100) { row in
-                NavigationLink("Row \(row)") {
-                    Text("Detail about row \(row)")
+        Button("Decode JSON") {
+            let input = """
+            {
+                "name": "Taylor Swift",
+                "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
                 }
             }
-            .navigationTitle("SwiftUI")
+            """
+
+            let data = Data(input.utf8)
+            let decoder = JSONDecoder()
+            if let user = try? decoder.decode(User.self, from: data) {
+                print(user.address.street)
+            }
         }
             
     }
