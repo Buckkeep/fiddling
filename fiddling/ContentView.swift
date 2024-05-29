aeedfb6e56b58416d7cc714c7420c09ceb66555f
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-29 13:28:20 +0100

    Returning to root view programmatically (using Binding and NagivationPath() )

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -4,28 +4,28 @@
 //
 //  Created by Neeta Buhecha on 01/05/2024.
 //
-//  Returning to root view programmatically (using Binding)
+//  Returning to root view programmatically (using Binding and NagivationPath() )
 //
 
 import SwiftUI
 
 struct DetailView: View {
     var number: Int
-    @Binding var path: [Int]
+    @Binding var path: NavigationPath
     
     var body: some View {
         NavigationLink("Go to random number", value: Int.random(in: 0...1000))
             .navigationTitle("Number: \(number)")
             .toolbar {
                 Button("Home") {
-                    path.removeAll()
+                    path = NavigationPath()
                 }
             }
     }
 }
 
 struct ContentView: View {
-    @State private var path = [Int]()
+    @State private var path = NavigationPath()
     
     var body: some View {
         NavigationStack(path: $path) {
