d9638143d66735aa49b04c0dd164433448635620
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-29 14:32:12 +0100

    Save NavigationStack paths using Codable - [Int]

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -4,23 +4,49 @@
 //
 //  Created by Neeta Buhecha on 01/05/2024.
 //
-//  Returning to root view programmatically (using Binding and NagivationPath() )
+//  Save NavigationStack paths using Codable - [Int]
 //
 
 import SwiftUI
 
+@Observable
+class PathStore {
+    var path: [Int] {
+        didSet {
+            save()
+        }
+    }
+    
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode([Int].self, from: data) {
+                path = decoded
+                return
+            }
+        }
+        // if you couldn't decode it/load the file the return will bring us here
+        path = []
+    }
+    
+    func save() {
+        do {
+            let data = try JSONEncoder().encode(path)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+    
+}
+
 struct DetailView: View {
     var number: Int
-    @Binding var path: NavigationPath
     
     var body: some View {
         NavigationLink("Go to random number", value: Int.random(in: 0...1000))
             .navigationTitle("Number: \(number)")
-            .toolbar {
-                Button("Home") {
-                    path = NavigationPath()
-                }
-            }
     }
 }
 
@@ -29,9 +55,9 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack(path: $path) {
-            DetailView(number: 0, path: $path)
+            DetailView(number: 0)
                 .navigationDestination(for: Int.self) {i in
-                    DetailView(number: i, path: $path)
+                    DetailView(number: i)
                 }
         }
     }
