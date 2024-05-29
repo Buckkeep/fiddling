168a94f4daf4448a9f0b4afa24632be068a3924a
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-29 11:09:13 +0100

    Handling navigation the smart way with navigationDestination()

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -4,32 +4,34 @@
 //
 //  Created by Neeta Buhecha on 01/05/2024.
 //
-//  LazyHGrid - Adaptive Row Sizes with Horizontal Scroll
+//  Handling navigation the smart way with navigationDestination()
 //
 
 import SwiftUI
 
-struct ContentView: View {
-    
-    let layout = [
-        GridItem(.adaptive(minimum: 80, maximum: 120)),
-    ]
-                 
-    
-    var body: some View {
-        ScrollView(.horizontal) {
-            LazyHGrid(rows: layout) {
-                ForEach(0..<1000) {
-                    Text("Item \($0)")
-                }
-            }
-        }
-            
-    }
 
-
+struct Student: Hashable {
+    let id = UUID()
+    let name: String
+    let age: Int
 }
 
+struct ContentView: View {
+    
+    var body: some View {
+        NavigationStack{
+            List(0..<100) { i in
+                NavigationLink("Select \(i)", value: i)
+            }
+            .navigationDestination(for: Int.self) {selection in
+                Text("You selected \(selection)")
+            }
+        }
+        
+    }
+}
+
+    
 #Preview {
     ContentView()
 }
