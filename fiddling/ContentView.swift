7ec2ab20cb030aeada47d98dae34700715994aa3
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-07-05 10:01:32 +0100

    Day 49: Sending and receiving Codable data with URLSession and SwiftUI

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -9,14 +9,49 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
-    @State private var title = "SwiftUI"
+    @State private var results = [Result]()
+    
 
     var body: some View {
-        NavigationStack {
-            Text("Hello, world!")
-                .navigationTitle($title)
-                .navigationBarTitleDisplayMode(.inline)
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                
+                Text(item.collectionName)
+            }
+        }
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data)
+            {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
         }
     }
 }
