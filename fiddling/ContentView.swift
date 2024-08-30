c4baaaa2f084dfef1b12ca3e7df841dbdfd0c503
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-08-30 15:46:13 +0100

    Twirl Distortion

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -28,10 +28,11 @@ struct ContentView: View {
         let beginImage = CIImage(image: inputImage)
         
         let context = CIContext()
-        let currentFilter = CIFilter.crystallize()
+        let currentFilter = CIFilter.twirlDistortion()
         
         currentFilter.inputImage = beginImage
-        currentFilter.radius = 10
+        currentFilter.radius = 300
+        currentFilter.center = CGPoint(x: inputImage.size.width/2, y: inputImage.size.height/2)
         
         guard let outputImage = currentFilter.outputImage else {return}
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {return}
