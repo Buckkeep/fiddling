b5833fe5b34610e56c8a138f16d09bb924efb095
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-08-30 15:41:58 +0100

    Pixellated effect

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -28,10 +28,10 @@ struct ContentView: View {
         let beginImage = CIImage(image: inputImage)
         
         let context = CIContext()
-        let currentFilter = CIFilter.sepiaTone()
+        let currentFilter = CIFilter.pixellate()
         
         currentFilter.inputImage = beginImage
-        currentFilter.intensity = 1
+        currentFilter.scale = 7
         
         guard let outputImage = currentFilter.outputImage else {return}
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {return}
