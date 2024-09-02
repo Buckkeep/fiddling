37102ea9a90f1a541535615f2f857e51cdeca706
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-09-02 09:03:10 +0100

    Domnostrating filters with older API

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -28,11 +28,23 @@ struct ContentView: View {
         let beginImage = CIImage(image: inputImage)
         
         let context = CIContext()
-        let currentFilter = CIFilter.twirlDistortion()
+        let currentFilter = CIFilter.sepiaTone()
         
         currentFilter.inputImage = beginImage
-        currentFilter.radius = 300
-        currentFilter.center = CGPoint(x: inputImage.size.width/2, y: inputImage.size.height/2)
+        let amount = 1.0
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
+        }
+        
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)
+        }
+        
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
+        }
         
         guard let outputImage = currentFilter.outputImage else {return}
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {return}
