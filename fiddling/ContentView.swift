5239db20acdffe3e1e32fccfdab3c082d1a2aaf6
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-09-02 10:10:28 +0100

    Add multiple photos (limited to 3 in maxSelectionCount). Scrollview added to view all photos.

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -11,19 +11,30 @@ import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    @State private var pickerItem: PhotosPickerItem?
-    @State private var selectedImage: Image?
+    @State private var pickerItems = [PhotosPickerItem]()
+    @State private var selectedImages = [Image]()
     
     var body: some View {
         VStack {
-            PhotosPicker("Select a picture", selection: $pickerItem, matching: .images)
-            selectedImage?
-                .resizable()
-                .scaledToFit()
-        }
-        .onChange(of: pickerItem) {
-            Task {
-                selectedImage = try await pickerItem?.loadTransferable(type: Image.self)
+            PhotosPicker("Select a picture", selection: $pickerItems, maxSelectionCount: 3, matching: .images)
+            
+            
+            ScrollView {
+                ForEach(0..<selectedImages.count, id: \.self) { i in
+                    selectedImages[i]
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            .onChange(of: pickerItems) {
+                Task {
+                    selectedImages.removeAll()
+                    for item in pickerItems {
+                        if let loadedImage = try await item.loadTransferable(type: Image.self) {
+                            selectedImages.append(loadedImage)
+                        }
+                    }
+                }
             }
         }
     }
