feaf14387b331123a1b48b26c1789710c322ff70
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-09-02 10:53:26 +0100

    How to let the user share content with ShareLink (with preview)

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -4,18 +4,19 @@
 //
 //  Created by Neeta Buhecha on 01/05/2024.
 //
-//  How to let the user share content with ShareLink
+//  How to let the user share content with ShareLink (with preview)
 //
 
-import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    @State private var pickerItems = [PhotosPickerItem]()
-    @State private var selectedImages = [Image]()
     
     var body: some View {
-        ShareLink(item: URL(string: "https://www.hackingwithswift.com")!, subject: Text("Learn Swift Here"), message: Text("Check out 100 Days of SwiftUI"))
+        let frodo = Image(.frodo)
+        
+        ShareLink(item: frodo, preview: SharePreview("A Baggins", image: frodo)) {
+        Label("Click to share", systemImage: "airplane")
+        }
     }
 }
 
