83ca6edc6a9a47570d073af2e1bcdaf709991d6f
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-09-04 10:44:46 +0100

    Writing data to the documents directory

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -4,31 +4,25 @@
 //
 //  Created by Neeta Buhecha on 03/09/2024.
 //
-//  Adding conformance to Comparable for custom types
+//  Writing data to the documents directory
 //
 
 import SwiftUI
 
-struct User: Comparable, Identifiable {
-    let id = UUID()
-    var firstName: String
-    var lastName: String
-    
-    static func <(lhs: User, rhs: User) -> Bool {
-        lhs.lastName < rhs.lastName
-    }
-}
-
 struct ContentView: View {
-    let users = [
-        User(firstName: "Arnold", lastName: "Rimmer"),
-        User(firstName: "Kristine", lastName: "Kochanski"),
-        User(firstName: "David", lastName: "Lister"),
-    ].sorted()
 
     var body: some View {
-        List(users) { user in
-            Text("\(user.lastName), \(user.firstName)")
+        Button("Read and Write") {
+            let data = Data("Test Message".utf8)
+            let url = URL.documentsDirectory.appending(path: "message.txt")
+
+            do {
+                try data.write(to: url, options: [.atomic, .completeFileProtection])
+                let input = try String(contentsOf: url)
+                print(input)
+            } catch {
+                print(error.localizedDescription)
+            }
         }
     }
 }
