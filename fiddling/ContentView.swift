389763728a33a171fc02361475c9ecedf55626c9
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-09-04 10:15:23 +0100

    Adding conformance to Comparable for custom types - added Comparable conformance to User struct

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -9,14 +9,30 @@
 
 import SwiftUI
 
+struct User: Comparable, Identifiable {
+    let id = UUID()
+    var firstName: String
+    var lastName: String
+    
+    static func <(lhs: User, rhs: User) -> Bool {
+        lhs.lastName < rhs.lastName
+    }
+}
+
 struct ContentView: View {
-    let values = [1,5,3,6,2,9].sorted()
+    let users = [
+        User(firstName: "Arnold", lastName: "Rimmer"),
+        User(firstName: "Kristine", lastName: "Kochanski"),
+        User(firstName: "David", lastName: "Lister"),
+    ].sorted()
+
     var body: some View {
-        List(values, id: \.self) {
-            Text(String($0))
+        List(users) { user in
+            Text("\(user.lastName), \(user.firstName)")
         }
     }
 }
+
 
     
 #Preview {
