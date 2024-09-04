bd0e2696d77a30adb54c2acfd426eb63485b5614
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-09-04 11:35:07 +0100

    Mark London as initial position by passing in a variable

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -11,9 +11,16 @@ import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    
+    let position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        )
+    )
 
     var body: some View {
-        Map()
+        Map(initialPosition: position)
             .mapStyle(.hybrid)
     }
 }
