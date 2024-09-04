e228ed2fc3036b44e1405b5f37258eb236c4b2b0
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-09-04 14:15:19 +0100

    Using TouchID and FaceID with SwiftUI

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -4,41 +4,48 @@
 //
 //  Created by Neeta Buhecha on 04/09/2024.
 //
-//  Integrating MapKit with SwiftUI
+//  Using TouchID and FaceID with SwiftUI
 //
 
-import MapKit
+import LocalAuthentication
 import SwiftUI
 
 struct ContentView: View {
     
-    struct Location: Identifiable {
-        let id = UUID()
-        var name: String
-        var coordinate: CLLocationCoordinate2D
-    }
-    
-    let locations = [
-        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-    ]
-
+    @State private var isUnlocked = false
     
     var body: some View {
         VStack {
-            MapReader { proxy in
-                Map()
-                    .onTapGesture { position in
-                        if let coordinate = proxy.convert(position, from: .local) {
-                            print(coordinate)
-                        }
-                    }
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
             }
-
-
         }
+        .onAppear(perform: authenticate)
     }
     
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "We need to unlock your data."
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                // authentication has now completed
+                if success {
+                    isUnlocked = true
+                } else {
+                    // there was a problem
+                }
+            }
+        } else {
+            // no biometrics
+        }
+    }
 }
 
 
