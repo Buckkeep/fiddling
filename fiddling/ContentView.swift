2bde9f7fb39e563a42071ede67f31cd325fe7158
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-09-04 11:39:58 +0100

    Update location with two way binding, buttons allowing different positions (Paris and Tokyo)

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -12,7 +12,7 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let position = MapCameraPosition.region(
+    @State private var position = MapCameraPosition.region(
         MKCoordinateRegion(
             center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
             span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
@@ -20,9 +20,33 @@ struct ContentView: View {
     )
 
     var body: some View {
-        Map(initialPosition: position)
-            .mapStyle(.hybrid)
+        VStack {
+            Map(position: $position)
+                .mapStyle(.hybrid)
+            HStack(spacing: 50) {
+                Button("Paris") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
+                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                        )
+                    )
+                }
+
+                Button("Tokyo") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(
+                            center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
+                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+                        )
+                    )
+                }
+            }
+        }
     }
+    
+    
+    
 }
 
 
