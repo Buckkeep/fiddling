f99d27f521a62469f131d7050d6f94e61e8ed026
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-09-04 11:54:46 +0100

    Annotating the map with standard pins

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -12,44 +12,28 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var position = MapCameraPosition.region(
-        MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
-            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-        )
-    )
-
-    var body: some View {
-        VStack {
-            Map(position: $position)
-                .mapStyle(.hybrid)
-                .onMapCameraChange(frequency: .continuous) { context in
-                    print(context.region)
-                }
-            
-            HStack(spacing: 50) {
-                Button("Paris") {
-                    position = MapCameraPosition.region(
-                        MKCoordinateRegion(
-                            center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
-                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                        )
-                    )
-                }
-
-                Button("Tokyo") {
-                    position = MapCameraPosition.region(
-                        MKCoordinateRegion(
-                            center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
-                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                        )
-                    )
-                }
-            }
-        }
+    struct Location: Identifiable {
+        let id = UUID()
+        var name: String
+        var coordinate: CLLocationCoordinate2D
     }
     
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+
     
+    var body: some View {
+        VStack {
+            Map {
+                ForEach(locations) { location in
+                    Marker(location.name, coordinate: location.coordinate)
+                }
+            }
+
+        }
+    }
     
 }
 
