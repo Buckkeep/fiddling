19cb72f5f7d460d98687f80eee1a78fb5bd4422f
Neeta Buhecha <neeta.buhecha@natwest.com>
2025-01-28 15:32:05 +0000

    How to use gestures in SwiftUI - rotationEffect

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -10,25 +10,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var currentAmount = 0.0
-    @State private var finalAmount = 1.0
-    
+    @State private var currentAmount = Angle.zero
+    @State private var finalAmount = Angle.zero
+
     var body: some View {
-        Text("Hello, world!")
-            .scaleEffect(finalAmount + currentAmount)
+        Text("Hello, World!")
+            .rotationEffect(currentAmount + finalAmount)
             .gesture(
-                MagnifyGesture()
+                RotateGesture()
                     .onChanged { value in
-                        currentAmount = value.magnification - 1
+                        currentAmount = value.rotation
                     }
                     .onEnded { value in
                         finalAmount += currentAmount
-                        currentAmount = 0
+                        currentAmount = .zero
                     }
             )
     }
-    
 }
 
 
