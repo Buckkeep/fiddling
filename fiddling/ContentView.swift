cca29b62fef8010122b6fdcdd999f790bf2d48be
Neeta Buhecha <neeta.buhecha@natwest.com>
2025-01-28 13:39:15 +0000

    How to use gestures in SwiftUI - Magnify Gesture onChanged and onEnded

--- fiddling/ContentView.swift
+++ fiddling/ContentView.swift
@@ -2,50 +2,33 @@
 //  ContentView.swift
 //  fiddling
 //
-//  Created by Neeta Buhecha on 04/09/2024.
+//  Created by Neeta Buhecha on 13/01/2025.
 //
-//  Using TouchID and FaceID with SwiftUI
+//  How to use gestures in SwiftUI
 //
 
-import LocalAuthentication
 import SwiftUI
 
 struct ContentView: View {
     
-    @State private var isUnlocked = false
+    @State private var currentAmount = 0.0
+    @State private var finalAmount = 1.0
     
     var body: some View {
-        VStack {
-            if isUnlocked {
-                Text("Unlocked")
-            } else {
-                Text("Locked")
-            }
-        }
-        .onAppear(perform: authenticate)
+        Text("Hello, world!")
+            .scaleEffect(finalAmount + currentAmount)
+            .gesture(
+            MagnifyGesture()
+                .onChanged { value in
+                    currentAmount = value.magnification - 1
+                }
+                .onEnded { value in
+                    finalAmount += currentAmount
+                    currentAmount = 0
+                    }
+            )
     }
     
-    func authenticate() {
-        let context = LAContext()
-        var error: NSError?
-
-        // check whether biometric authentication is possible
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            // it's possible, so go ahead and use it
-            let reason = "We need to unlock your data."
-
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
-                // authentication has now completed
-                if success {
-                    isUnlocked = true
-                } else {
-                    // there was a problem
-                }
-            }
-        } else {
-            // no biometrics
-        }
-    }
 }
 
 
